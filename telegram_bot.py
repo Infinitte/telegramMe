73c4b160b2721899de5e75071f2747e4882c20d3
Infinitte <lorenzo.ortiz@gmail.com>
2023-06-08 19:42:36 +0200

    Actions

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,8 @@
+import os
 import requests
 
-
+BOT_TOKEN=os.environ["BOT_TOKEN"]
+CHAT_ID=os.environ["CHAT_ID"]
 
 def send_message(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
